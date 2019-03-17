7cd47983d8b284d005042f4624bcb9af9d530991
AlfredoQT1 <48574486+AlfredoQT1@users.noreply.github.com>
2019-03-17 13:47:16 -0700

    Add files via upload

---
+++ js/extra.js
@@ -0,0 +1,15 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
+function offsetAnchor() {
+    if (location.hash.length !== 0) {
+        window.scrollTo(window.scrollX, window.scrollY - 100);
+    }
+}
+$(document).on('click', 'a[href^="#"]', function(event) {
+    window.setTimeout(function() {
+        offsetAnchor();
+    }, 0);
+});
+window.setTimeout(offsetAnchor, 0);
